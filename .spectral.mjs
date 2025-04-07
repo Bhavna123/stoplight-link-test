c0797e32f0962e3d9857803779ffc7077df62d52
bhavana.bhaskar@smartbear.com <bhavana.bhaskar@smartbear.com>
2025-04-07 17:05:52 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3Nzc0MQ/spectral.js?branch_slug=main&token=c93dc00d-cafe-4f93-9bf2-7c35e72c1521";
+export default { extends: ruleset };
